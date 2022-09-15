cd316901e3ea7883ebc94e3224c6f81230ebdbd8
MohammadAjam1997 <ajamm9644@gmail.com>
2022-09-15 13:35:00 +0300

    improve

--- src/app/components/currency/currency.component.ts
+++ src/app/components/currency/currency.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { ConfigerService } from '../shared/services/configer.service'; 
 import { NgxImgZoomService } from "ngx-img-zoom";
 import { CurrencyStateService } from './currency-state.service';
 import { Observable } from 'rxjs';
